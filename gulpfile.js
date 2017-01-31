a05e2fb2b646515499734dd384f2650eb9886db6
jamisec <jami.sec15@gmail.com>
2017-01-31 12:01:15 -0800

    j

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp= require('gulp');
+    sass= require('gulp-sass');
+    pug= require('gulp-pug');
+
+
+
+gulp.task('sass',function () {
+    gulp.src('sass/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('bin/css/'));
+});
+gulp.task('pug',function () {
+    gulp.src('src/pug/pages/*.pug')
+        .pipe(pug({pretty:true}))
+        .pipe(gulp.dest('bin/html/'));
+});
+
+gulp.task('js',function () {
+    gulp.src('src/js/main.js')
+        .pipe(gulp.dest('bin/js/'));
+});
+
+gulp.task('movelib',function () {
+    gulp.src('node_modules/bootstrap/dist/css/**/*.min.css')
+        .pipe(gulp.dest('bin/final-product/lib/bootstrap/css'));
+    gulp.src('node_modules/bootstrap/dist/js/**/*.min.js')
+        .pipe(gulp.dest('bin/final-product/lib/bootstrap/js'));
+    gulp.src('node_modules/owlcarousel/owl-carousel/**/*.css')
+        .pipe(gulp.dest('bin/final-product/lib/owlcarousel/css'));
+    gulp.src('node_modules/owlcarousel/owl-carousel/**/*.min.js')
+        .pipe(gulp.dest('bin/final-product/lib/owlcarousel/js'));
+    gulp.src('node_modules/ionicons/dist/css/**/*.min.css')
+        .pipe(gulp.dest('bin/final-product/lib/ionicons/css'));
+    gulp.src('node_modules/ionicons/dist/fonts/*')
+        .pipe(gulp.dest('bin/final-product/lib/ionicons/fonts'));
+    gulp.src('node_modules/isotope/dist/**/*.min.css')
+        .pipe(gulp.dest('bin/final-product/lib/isotope/css'));
+    gulp.src('node_modules/jquery/dist/*.min.js')
+        .pipe(gulp.dest('bin/final-product/lib/jquery/'));
+
+});
+gulp.task('watchsass',function () {
+    gulp.watch("src/scss/**/*.scss",['sass']);
+});
+
+gulp.task('watchjs',function () {
+    gulp.watch("src/js/**/*.js",['js']);
+});
+gulp.task('watchpug',function () {
+    gulp.watch("src/pug/**/*.pug",['pug']);
+});
+
+gulp.task('default'),['sass','pug','watchsass','watchpug','js','watchjs'];
